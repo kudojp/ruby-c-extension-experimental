fc44da849fc9c49bd11fb1647af87cf2f1b035d1
kudojp <heyjudejudejude1968@gmail.com>
2022-01-08 15:10:30 +0900

    Implement #Init_foobar

--- foobar.c
+++ foobar.c
@@ -1,7 +1,12 @@
 #include "ruby.h"
 #include "extconf.h"
 
+VALUE rb_return_nil(){
+  return Qnil;
+}
+
 void Init_foobar() // This has to be the same the one declared in extconf.rb
-// {
-//   // my C code goes here.
-// }
+{
+  VALUE mod = rb_define_module("RubyGuides");
+  rb_define_method(mod, "return_nil", rb_return_nil, 0);
+}
